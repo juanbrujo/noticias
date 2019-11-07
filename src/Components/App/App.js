5ab1db5242b3821bbb6af5bc8e02c151c4062ed7
Jose Luis Gonzalez <jl.gonzalezmedel@gmail.com>
2019-11-07 10:19:44 -0300

    Fix signal and feed

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,29 +11,34 @@ class App extends React.Component {
   constructor () {
     super()
     this.signals = [{
-      title: '24 Horas TVN',
-      src: 'https://www.youtube.com/embed/g4599q5uxnM?autoplay=0&mute=0&vq=medium&enablejsapi=1&modestbranding=1&showinfo=0'
+      title: '24 Play',
+      src: 'https://mdstrm.com/live-stream/57d1a22064f5d85712b20dab?jsapi=true&autoplay=false&volume=100'
     }, {
-      title: 'T13',
+      title: 'T13 Mobil',
       src: 'https://www.youtube.com/embed/bOvhCYr1NYk?autoplay=0&mute=0&vq=medium&enablejsapi=1&modestbranding=1&showinfo=0'
-    }, {
-      title: 'Meganoticias',
-      src: 'https://www.youtube.com/embed/tUmseNWVu58?autoplay=0&mute=0&vq=medium&enablejsapi=1&modestbranding=1&showinfo=0'
-    }, {
-      title: 'CHV',
-      src: 'https://mdstrm.com/live-stream/5c6c302a414cc5496c9d7d79?jsapi=true&autoplay=false&volume=100'
     }, {
       title: 'CNN Chile',
       src: 'https://www.youtube.com/embed/y5vvZ5gbF10?autoplay=0&mute=0&vq=medium&enablejsapi=1&modestbranding=1&showinfo=0'
     }]
-    this.feeds = [
-      'https://rss.app/feeds/G7T7btEZ3kaIHrfj.xml',
-      'https://rss.app/feeds/cB5YJOsFis7lh7yb.xml',
-      'https://rss.app/feeds/PuWTsqczRrybKiWS.xml',
-      'https://rss.app/feeds/0uf5l2IhtBF9Qag8.xml',
-      'https://rss.app/feeds/o528mkszQo5rfGkv.xml',
-      'https://www.cooperativa.cl/noticias/site/tax/port/all/rss_3___1.xml'
-    ]
+    this.feeds = [{
+      title: 'Piensa Prensa',
+      url: 'https://www.serveruwu.com/api/twitter/PiensaPrensa'
+    }, {
+      title: 'Bio Bio Chile',
+      url: 'https://www.serveruwu.com/api/twitter/biobio'
+    }, {
+      title: 'INDH',
+      url: 'https://www.serveruwu.com/api/twitter/inddhh'
+    }, {
+      title: 'Prensa Opal',
+      url: 'https://www.serveruwu.com/api/twitter/prensaopal'
+    }, {
+      title: 'El Desconcierto',
+      url: 'https://www.serveruwu.com/api/twitter/eldesconcierto'
+    }, {
+      title: 'Cooperativa',
+      url: 'https://www.serveruwu.com/api/twitter/cooperativa'
+    }]
   }
 
   render () {
@@ -52,7 +57,7 @@ class App extends React.Component {
           <div className={headerClass}><Weather /></div>
           <div className={headerClass}><CountDown title={'Marcha Martes 29'} date={'Oct 29, 2019 17:30:00'} finish={'00 : 00 : 00'} /></div>
           {this.signals.map((signal, index) => <Signal key={index} title={signal.title} src={signal.src} />)}
-          {this.feeds.map((feed, index) => <Feed key={index} url={feed} />)}
+          {this.feeds.map((feed, index) => <Feed key={index} title={feed.title} url={feed.url} />)}
         </div>
       </div>
     )
--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,53 +1,65 @@
 import React from 'react'
+import https from 'https'
 import PropTypes from 'prop-types'
-import Parser from 'rss-parser'
 import _get from 'lodash/get'
-import { format } from 'date-fns'
-import { es } from 'date-fns/locale'
+// import { es } from 'date-fns/locale'
 import MySwiper from '../Swipper/Swipper'
+// import { format, parse } from 'date-fns'
 
 class Feed extends React.PureComponent {
   constructor () {
     super()
-    this.parser = new Parser()
     this.state = {
-      title: '',
       items: []
     }
     this.corsProxy = 'https://proxy.serveruwu.com/'
   }
 
   static propTypes = {
+    title: PropTypes.string,
     url: PropTypes.string
   }
 
   componentDidMount = async () => {
     const { url } = this.props
-    const feed = await this.parser.parseURL(this.corsProxy + url)
-    this.setState({
-      site: feed.title,
-      items: feed.items
+    https.get(this.corsProxy + url, res => {
+      res.setEncoding('utf8')
+      let body = ''
+      res.on('data', data => {
+        body += data
+      })
+      res.on('end', () => {
+        body = JSON.parse(body)
+        this.setState({
+          items: body
+        })
+      })
     })
   }
 
   renderDate (pubDate) {
-    return <span className='timetext-small'>{format(new Date(pubDate), 'HH:mm:ss MM/dd/yyyy', { locale: es })}</span>
+    // console.log('TCL: Feed -> renderDate -> pubDate', pubDate)
+    // const twitterDate = parse(pubDate, 'EEE, dd MMM yyyy HH:mm:ss [GMT]')
+    // console.log('TCL: Feed -> renderDate -> twitterDate', twitterDate)
+    return <span className='timetext-small'>{pubDate}</span>
   }
 
   render () {
-    const { site, items } = this.state
+    const { items } = this.state
+    const { title } = this.props
     const elements = items.map((item, index) => {
       const image = _get(item, 'enclosure', {})
       const imageUrl = _get(image, 'url')
       return <a className='rss-link' key={index} href={item.link} target="_blank" rel="noopener noreferrer">
         {imageUrl && <img src={imageUrl} alt="imagen"/>}
-        <span className='text'>{this.renderDate(item.pubDate)} - {item.title}</span>
+        <span className='text'>{this.renderDate(item.pubDate)}</span><br/>
+        <span className='text' style={{ color: '#ffcf77' }}>{item.description}</span>
         <span className='clear' />
       </a>
     })
 
     return <div className="col-12 col-sm-6 col-md-4 col-lg-4">
-      <h5 className='rss-site'>{site}</h5>
+      <h5 className='rss-site'>{title}</h5>
       <MySwiper slides={elements} />
     </div>
   }
